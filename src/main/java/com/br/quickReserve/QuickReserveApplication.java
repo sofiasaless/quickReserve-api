6f81db7c049c6bcff5dfed6c735d0ce9f85009f5
sofiasaless <sofiasaleswk@gmail.com>
2025-01-08 14:47:08 -0300

    commit inicial

---
+++ src/main/java/com/br/quickReserve/QuickReserveApplication.java
@@ -0,0 +1,13 @@
+package com.br.quickReserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/quickReserve/QuickReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.quickReserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
