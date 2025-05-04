9d37b55c3642dddc15a2dc7dd2b3b82725520eb0
sofiasaless <sofiasaleswk@gmail.com>
2025-05-04 02:13:54 -0300

    feat: metodo para encontrar um restaurante por id de uma mesa

--- src/main/java/com/br/quickReserve/repository/RestauranteRepository.java
+++ src/main/java/com/br/quickReserve/repository/RestauranteRepository.java
@@ -3,6 +3,8 @@ package com.br.quickReserve.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.br.quickReserve.model.RestauranteEntity;
 
@@ -10,4 +12,13 @@ public interface RestauranteRepository extends JpaRepository<RestauranteEntity, 
     Optional<RestauranteEntity> findByEmail(String email);
     Optional<RestauranteEntity> findByCnpj(String cnpj);
     Optional<RestauranteEntity> findByCnpjOrEmail(String cnpj, String email);
+
+    @Query(
+        value = "SELECT rt.* FROM mesas me " +
+            "JOIN restaurantes rt ON me.restaurante_id = rt.id " +
+            "WHERE me.id = :mesaId " +
+            "LIMIT 1", 
+        nativeQuery = true
+    )
+    Optional<RestauranteEntity> findByMesaId(@Param("mesaId") Long mesaId);
 }
